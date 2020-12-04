02b8ee29a2bd7daab98576f72656b8ae45a04d8f
Ben Watson <benwatson528@gmail.com>
2020-12-04 09:13:17 +0000

    Day 4

--- main/day04/passport_processing.py
+++ main/day04/passport_processing.py
@@ -1,16 +1,42 @@
-from typing import Set
+import re
+
+required = {"byr": r"^([1][9][2-9][0-9])|([2][0][0][0-2])$",
+            "iyr": "^([2][0][1][0-9])|([2][0][2][0])$",
+            "eyr": r"^([2][0][2][0-9])|([2][0][3][0])$",
+            "hgt": r"^([1]([5-8][0-9]|[9][0-3])cm)|(([5][9]|[6][0-9]|[7][0-6])in)$",
+            "hcl": r"^#(\d|[a-f]){6}$",
+            "ecl": r"^(amb|blu|brn|gry|grn|hzl|oth)$",
+            "pid": r"^\d{9}$"}
+
+optional = {"cid"}
 
 
-def solve(data: str, required: Set[str], optional: Set[str]) -> int:
+def solve(data: str, validate=False) -> int:
     passports = data.split("\n\n")
     valid_passports = 0
     for passport in passports:
-        extracted_fields = {}
-        for parsed in passport.replace("\n", " ").split(" "):
-            if ":" not in parsed:
-                continue
-            key, value = parsed.split(":")
-            extracted_fields[key] = value
-        if required.issubset(extracted_fields.keys()):
+        extracted_fields = extract_fields(passport)
+        if set(required.keys()).issubset(extracted_fields.keys()) and validate_values(extracted_fields,
+                                                                                      validate):
             valid_passports += 1
     return valid_passports
+
+
+def extract_fields(passport):
+    extracted_fields = {}
+    for parsed in passport.replace("\n", " ").split(" "):
+        if ":" not in parsed:
+            continue
+        key, value = parsed.split(":")
+        extracted_fields[key] = value
+    return extracted_fields
+
+
+def validate_values(extracted_fields, validate):
+    if not validate:
+        return True
+    else:
+        for extracted_field, value in extracted_fields.items():
+            if extracted_field not in optional and not re.match(required[extracted_field], value):
+                return False
+        return True
--- tests/day04/test_passport_processing.py
+++ tests/day04/test_passport_processing.py
@@ -6,12 +6,22 @@ from main.day04.passport_processing import solve
 
 def test_simple():
     data = read_input("data/test_input.txt")
-    assert solve(data, {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}, {"cid"}) == 2
+    assert solve(data) == 2
 
 
 def test_real():
     data = read_input("data/input.txt")
-    assert solve(data, {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}, {"cid"}) == 170
+    assert solve(data) == 170
+
+
+def test_validation_simple():
+    data = read_input("data/validated_test_input.txt")
+    assert solve(data, True) == 4
+
+
+def test_validation_real():
+    data = read_input("data/input.txt")
+    assert solve(data, True) == 103
 
 
 def read_input(file_name):
