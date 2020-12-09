aa182145f288aa5ac5998b3f9612726cad41498e
Ben Watson <benwatson528@gmail.com>
2020-12-09 08:38:29 +0000

    Day 9 part 2

--- main/day09/encoding_error.py
+++ main/day09/encoding_error.py
@@ -1,14 +1,40 @@
 import itertools
-from typing import List
+from typing import List, Optional, Callable
 
 
-def solve(nums: List[int], preamble_size: int) -> int:
-    queue = nums[:preamble_size]
+def solve_any_two(nums: List[int], preamble_size: int) -> Optional[int]:
+    return solve(nums, preamble_size, find_any_two)[0]
+
+
+def solve_contiguous(nums: List[int], target: int) -> Optional[List[int]]:
+    for i in range(len(nums)):
+        contiguous_sum = set()
+        for j in range(i, len(nums)):
+            contiguous_sum.add(nums[j])
+            if sum(contiguous_sum) == target:
+                return [min(contiguous_sum), max(contiguous_sum)]
+            elif sum(contiguous_sum) > target:
+                break
+    return None
+
+
+def find_any_two(preamble: List[int], target: int) -> Optional[List[int]]:
+    combinations = itertools.combinations(preamble, 2)
+    valid_combination = next((x for x in combinations if x[0] + x[1] == target), None)
+    if not valid_combination:
+        return [target]
+    else:
+        return None
+
+
+def solve(nums: List[int], preamble_size: int,
+          combination_evaluator: Callable[[List[int], int], Optional[List[int]]]) -> Optional[List[int]]:
+    preamble = nums[:preamble_size]
     codes = nums[preamble_size:]
     for num in codes:
-        combinations = list(itertools.combinations(queue, 2))
-        valid_combination = next((x for x in combinations if x[0] + x[1] == num), None)
-        if not valid_combination:
-            return num
-        queue.pop(0)
-        queue.append(num)
+        combination = combination_evaluator(preamble, num)
+        if combination:
+            return combination
+        preamble.pop(0)
+        preamble.append(num)
+    return None
--- tests/day09/test_encoding_error.py
+++ tests/day09/test_encoding_error.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day09.encoding_error import solve
+from main.day09.encoding_error import solve_any_two, solve_contiguous
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt"), 5) == 127
+def test_any_two_simple():
+    assert solve_any_two(read_input("data/test_input.txt"), 5) == 127
 
 
-def test_real():
-    assert solve(read_input("data/input.txt"), 25) == 731031916
+def test_any_two_real():
+    assert solve_any_two(read_input("data/input.txt"), 25) == 731031916
+
+
+def test_contiguous_simple():
+    assert sum(solve_contiguous(read_input("data/test_input.txt"), 127)) == 62
+
+
+def test_contiguous_real():
+    assert sum(solve_contiguous(read_input("data/input.txt"), 731031916)) == 93396727
 
 
 def read_input(file_name):
