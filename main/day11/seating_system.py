c8cc77062d5846cab5cc1535191406a22ac8939a
Ben Watson <benwatson528@gmail.com>
2020-12-11 14:35:13 +0000

    Day 11

--- main/day11/seating_system.py
+++ main/day11/seating_system.py
@@ -1,55 +1,161 @@
-from typing import List, Optional, Callable
+import operator
+from typing import Callable, Set
 
-Grid = List[str]
+AdjacencyFunction = Callable
 
 
-def solve(grid: Grid) -> int:
-    updated_grid = update_grid(grid, count_adjacent_tiles)
-    if updated_grid == grid:
-        tiles = count_all_tiles(updated_grid, '#')
-        return tiles
+def solve_adjacent(empty_seats: Set, occupied_seats: Set) -> int:
+    return solve(empty_seats, occupied_seats, count_adjacent_tiles, 4)
+
+
+def solve_visible(empty_seats: Set, occupied_seats: Set) -> int:
+    return solve(empty_seats, occupied_seats, count_visible_tiles, 5)
+
+
+def solve(empty_seats: Set, occupied_seats: Set, adjacency_function: AdjacencyFunction,
+          empty_threshold: int) -> int:
+    updated_empty_seats, updated_occupied_seats = update_grid(empty_seats, occupied_seats, adjacency_function,
+                                                              empty_threshold)
+    if updated_empty_seats == empty_seats and updated_occupied_seats == occupied_seats:
+        return len(updated_occupied_seats)
     else:
-        return solve(updated_grid)
+        return solve(updated_empty_seats, updated_occupied_seats, adjacency_function, empty_threshold)
 
 
-def update_grid(grid: Grid, adjacency_function: Callable[[Grid, str, int, int], int]) -> List[str]:
-    updated_grid = []
-    for i in range(len(grid)):
-        updated_grid.append("")
-        for j in range(len(grid[0])):
-            current_tile = grid[i][j]
-            if current_tile == 'L' and adjacency_function(grid, '#', i, j) == 0:
-                updated_grid[i] += '#'
-            elif current_tile == '#' and adjacency_function(grid, '#', i, j) >= 4:
-                updated_grid[i] += 'L'
-            else:
-                updated_grid[i] += current_tile
-    return updated_grid
+def update_grid(empty_seats: Set, occupied_seats: Set, adjacency_function: AdjacencyFunction,
+                empty_threshold: int) -> (Set, Set):
+    updated_empty_seats = set()
+    updated_occupied_seats = set()
+    for empty_seat in empty_seats:
+        if adjacency_function(empty_seats, occupied_seats, empty_seat, operator.eq, 0):
+            updated_occupied_seats.add(empty_seat)
+        else:
+            updated_empty_seats.add(empty_seat)
+
+    for occupied_seat in occupied_seats:
+        if adjacency_function(empty_seats, occupied_seats, occupied_seat, operator.ge, empty_threshold):
+            updated_empty_seats.add(occupied_seat)
+        else:
+            updated_occupied_seats.add(occupied_seat)
+    return updated_empty_seats, updated_occupied_seats
 
 
-def count_all_tiles(grid: Grid, tile: str) -> int:
-    num_seats = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == tile:
-                num_seats += 1
-    return num_seats
-
-
-def count_adjacent_tiles(grid: Grid, tile: str, x: int, y: int) -> int:
+def count_adjacent_tiles(empty_seats: Set, occupied_seats: Set, coords: (int, int), equality_operator: operator,
+                         threshold: int) -> bool:
+    x, y = coords
     num_tiles = 0
     for i in range(x - 1, x + 2):
         for j in range(y - 1, y + 2):
-            not_current = not (i == x and j == y)
-            found_tile = get_tile(grid, i, j)
-            is_desired_tile = found_tile == tile
-            if not_current and is_desired_tile:
+            if not (i == x and j == y) and (i, j) in occupied_seats:
                 num_tiles += 1
-    return num_tiles
+    return equality_operator(num_tiles, threshold)
 
 
-def get_tile(grid: Grid, x: int, y: int) -> Optional[str]:
-    if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
-        return None
-    else:
-        return grid[x][y]
+def count_visible_tiles(empty_seats: Set, occupied_seats: Set, coords: (int, int), equality_operator: operator,
+                        threshold: int) -> bool:
+    x, y = coords
+    num_tiles = 0
+    max_x, max_y = get_grid_bounds(empty_seats, occupied_seats)
+    num_tiles += walk_right(empty_seats, occupied_seats, max_x, x, y)
+    num_tiles += walk_left(empty_seats, occupied_seats, x, y)
+    num_tiles += walk_down(empty_seats, occupied_seats, x, y)
+    num_tiles += walk_up(empty_seats, occupied_seats, max_y, x, y)
+    num_tiles += walk_diagonally_down_left(empty_seats, occupied_seats, max_x, max_y, x, y)
+    num_tiles += walk_diagonally_down_right(empty_seats, occupied_seats, max_x, x, y)
+    num_tiles += walk_diagonally_up_left(empty_seats, occupied_seats, max_y, x, y)
+    num_tiles += walk_diagonally_up_right(empty_seats, occupied_seats, x, y)
+    return equality_operator(num_tiles, threshold)
+
+
+def walk_right(empty_seats, occupied_seats, max_x, x, y):
+    for i in range(x + 1, max_x + 1):
+        if (i, y) in empty_seats:
+            break
+        elif (i, y) in occupied_seats:
+            return 1
+    return 0
+
+
+def walk_left(empty_seats, occupied_seats, x, y):
+    for i in reversed(range(0, x)):
+        if (i, y) in empty_seats:
+            return 0
+        elif (i, y) in occupied_seats:
+            return 1
+    return 0
+
+
+def walk_down(empty_seats, occupied_seats, x, y):
+    for j in reversed(range(0, y)):
+        if (x, j) in empty_seats:
+            return 0
+        elif (x, j) in occupied_seats:
+            return 1
+    return 0
+
+
+def walk_up(empty_seats, occupied_seats, max_y, x, y):
+    for j in range(y + 1, max_y + 1):
+        if (x, j) in empty_seats:
+            return 0
+        elif (x, j) in occupied_seats:
+            return 1
+    return 0
+
+
+def walk_diagonally_down_left(empty_seats, occupied_seats, max_x, max_y, x, y):
+    i = x + 1
+    j = y + 1
+    while i < max_x + 1 and j < max_y + 1:
+        if (i, j) in empty_seats:
+            return 0
+        elif (i, j) in occupied_seats:
+            return 1
+        i += 1
+        j += 1
+    return 0
+
+
+def walk_diagonally_down_right(empty_seats, occupied_seats, max_x, x, y):
+    i = x + 1
+    j = y - 1
+    while i < max_x + 1 and j >= 0:
+        if (i, j) in empty_seats:
+            return 0
+        elif (i, j) in occupied_seats:
+            return 1
+        i += 1
+        j -= 1
+    return 0
+
+
+def walk_diagonally_up_left(empty_seats, occupied_seats, max_y, x, y):
+    i = x - 1
+    j = y + 1
+    while i >= 0 and j < max_y + 1:
+        if (i, j) in empty_seats:
+            return 0
+        elif (i, j) in occupied_seats:
+            return 1
+        i -= 1
+        j += 1
+    return 0
+
+
+def walk_diagonally_up_right(empty_seats, occupied_seats, x, y):
+    i = x - 1
+    j = y - 1
+    while i >= 0 and j >= 0:
+        if (i, j) in empty_seats:
+            return 0
+        elif (i, j) in occupied_seats:
+            return 1
+        i -= 1
+        j -= 1
+    return 0
+
+
+def get_grid_bounds(empty_seats: Set, occupied_seats: Set):
+    max_x = max(set(map(lambda x: x[0], empty_seats)).union(set(map(lambda x: x[0], occupied_seats))))
+    max_y = max(set(map(lambda x: x[1], empty_seats)).union(set(map(lambda x: x[1], occupied_seats))))
+    return max_x, max_y
--- tests/day11/test_seating_system.py
+++ tests/day11/test_seating_system.py
@@ -1,20 +1,61 @@
+import operator
 import os
 from pathlib import Path
+from typing import Set
 
-from main.day11.seating_system import solve
+import pytest
 
-
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 37
-
-
-def test_real():
-    assert solve(read_input("data/input.txt")) == 2468
+from main.day11.seating_system import solve_adjacent, solve_visible, count_visible_tiles
 
 
-def read_input(file_name):
+def test_adjacent_simple():
+    empty_seats, occupied_seats = read_input("data/test_input.txt")
+    assert solve_adjacent(empty_seats, occupied_seats) == 37
+
+
+def test_adjacent_real():
+    empty_seats, occupied_seats = read_input("data/input.txt")
+    assert solve_adjacent(empty_seats, occupied_seats) == 2468
+
+
+def test_visible_diagonal():
+    empty_seats, occupied_seats = read_input("data/small_diagonal.txt")
+    assert count_visible_tiles(empty_seats, occupied_seats, (3, 4), operator.eq, 8)
+
+
+def test_visible_no_occupied():
+    empty_seats, occupied_seats = read_input("data/small_no_occupied.txt")
+    assert count_visible_tiles(empty_seats, occupied_seats, (3, 3), operator.eq, 0)
+
+
+def test_visible_one_empty():
+    empty_seats, occupied_seats = read_input("data/small_one_empty.txt")
+    assert count_visible_tiles(empty_seats, occupied_seats, (1, 1), operator.eq, 0)
+
+
+def test_visible_simple():
+    empty_seats, occupied_seats = read_input("data/test_input.txt")
+    assert solve_visible(empty_seats, occupied_seats) == 26
+
+
+@pytest.mark.skip(reason="This takes fifteen minutes to complete")
+def test_visible_real():
+    empty_seats, occupied_seats = read_input("data/input.txt")
+    assert solve_visible(empty_seats, occupied_seats) == 2214
+
+
+def read_input(file_name) -> (Set, Set):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         lines = []
         for line in f:
             lines.append(line.strip('\n'))
-        return lines
+
+    empty = set()
+    occupied = set()
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == 'L':
+                empty.add((j, i))
+            elif lines[i][j] == '#':
+                occupied.add((j, i))
+    return empty, occupied
