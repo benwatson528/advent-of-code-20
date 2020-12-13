b2470f547ceb6e8fd0bf2f4c44de364ee116c738
Ben Watson <benwatson528@gmail.com>
2020-12-13 13:11:02 +0000

    Day 13 part 2

--- main/day13/shuttle_search.py
+++ main/day13/shuttle_search.py
@@ -1,6 +1,41 @@
+from functools import reduce
 from typing import List
 
 
-def solve(time: int, bus_ids: List[int]) -> int:
+def solve_first_bus(time: int, bus_ids: List[int]) -> int:
     min_time, min_bus_id = min(map(lambda x: (x - (time % x), x), bus_ids), key=lambda x: x[0])
     return min_time * min_bus_id
+
+
+def solve_consecutive_bus(bus_ids: List[int]) -> int:
+    valid_buses = []
+    indices = []
+    for i in range(len(bus_ids)):
+        if bus_ids[i] != "x":
+            valid_buses.append(int(bus_ids[i]))
+            indices.append(-i)
+    return chinese_remainder(valid_buses, indices)
+
+
+#  https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
+def chinese_remainder(n, a):
+    remainder_sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        remainder_sum += a_i * mul_inv(p, n_i) * p
+    return remainder_sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
--- tests/day13/test_shuttle_search.py
+++ tests/day13/test_shuttle_search.py
@@ -2,21 +2,60 @@ import os
 import re
 from pathlib import Path
 
-from main.day13.shuttle_search import solve
+from main.day13.shuttle_search import solve_first_bus, solve_consecutive_bus
 
 
-def test_simple():
-    time, bus_ids = read_input("data/test_input.txt")
-    assert solve(time, bus_ids) == 295
+def test_first_bus_simple():
+    time, bus_ids = read_part_one_input("data/test_input.txt")
+    assert solve_first_bus(time, bus_ids) == 295
 
 
-def test_real():
-    time, bus_ids = read_input("data/input.txt")
-    assert solve(time, bus_ids) == 370
+def test_first_bus_real():
+    time, bus_ids = read_part_one_input("data/input.txt")
+    assert solve_first_bus(time, bus_ids) == 370
 
 
-def read_input(file_name):
+def test_consecutive_bus_simple_one():
+    assert solve_consecutive_bus(parse_whole_line("7,13,x,x,59,x,31,19")) == 1068781
+
+
+def test_consecutive_bus_simple_two():
+    assert solve_consecutive_bus(parse_whole_line("17,x,13,19")) == 3417
+
+
+def test_consecutive_bus_simple_three():
+    assert solve_consecutive_bus(parse_whole_line("67,7,59,61")) == 754018
+
+
+def test_consecutive_bus_simple_four():
+    assert solve_consecutive_bus(parse_whole_line("67,x,7,59,61")) == 779210
+
+
+def test_consecutive_bus_simple_five():
+    assert solve_consecutive_bus(parse_whole_line("67,7,x,59,61")) == 1261476
+
+
+def test_consecutive_bus_simple_six():
+    assert solve_consecutive_bus(parse_whole_line("1789,37,47,1889")) == 1202161486
+
+
+def test_consecutive_bus_real():
+    bus_ids = read_part_two_input("data/input.txt")
+    assert solve_consecutive_bus(bus_ids) == 894954360381385
+
+
+def read_part_one_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         time = int(f.readline().strip("\n"))
         bus_ids = [int(s) for s in re.findall(r'\d+', f.readline().strip("\n"))]
         return time, bus_ids
+
+
+def read_part_two_input(file_name):
+    with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
+        f.readline()
+        return parse_whole_line(f.readline())
+
+
+def parse_whole_line(line):
+    return [s for s in line.strip("\n").split(",")]
