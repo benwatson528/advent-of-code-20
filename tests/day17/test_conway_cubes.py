2e96cb13fd5d223927bbeca76c0e11d96ef3def4
Ben Watson <benwatson528@gmail.com>
2020-12-17 09:01:35 +0000

    Commenting out long tests

--- tests/day17/test_conway_cubes.py
+++ tests/day17/test_conway_cubes.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from typing import Set
 
+import pytest
+
 from main.day17.conway_cubes import solve_3d, solve_4d
 from main.day17.coord import Coord
 
@@ -14,10 +16,12 @@ def test_3d_real():
     assert solve_3d(read_input("data/input.txt")) == 265
 
 
+@pytest.mark.skip(reason="Takes about 6s")
 def test_4d_simple():
     assert solve_4d(read_input("data/test_input.txt")) == 848
 
 
+@pytest.mark.skip(reason="Takes about 12s")
 def test_4d_real():
     assert solve_4d(read_input("data/input.txt")) == 1936
 
