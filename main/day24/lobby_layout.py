d92e09e975bcaa6cf936e17c69af1861ea99c837
Ben Watson <benwatson528@gmail.com>
2020-12-24 14:14:36 +0000

    Day 24

--- main/day24/lobby_layout.py
+++ main/day24/lobby_layout.py
@@ -2,21 +2,43 @@ from dataclasses import dataclass
 from typing import List, Set
 
 
-def solve(tiles: List[List[str]]) -> int:
+@dataclass(frozen=True)
+class Coord:
+    x: int
+    y: int
+
+
+def solve_place_tiles(tiles: List[List[str]]) -> Set[Coord]:
     flipped_tiles = set()
     for tile in tiles:
         coord = Coord(0, 0)
         for movement in tile:
             coord = move(coord, movement)
         update_tiles(coord, flipped_tiles)
+    return flipped_tiles
 
-    return len(flipped_tiles)
+
+def solve_update_tiles(tiles: List[List[str]], num_days: int) -> Set[Coord]:
+    updated_flipped_tiles = solve_place_tiles(tiles)
+    for _ in range(num_days):
+        updated_flipped_tiles = progress_day(updated_flipped_tiles)
+    return updated_flipped_tiles
 
 
-@dataclass(frozen=True)
-class Coord:
-    x: int
-    y: int
+def progress_day(flipped_tiles: Set[Coord]):
+    updated_flipped_tiles = flipped_tiles.copy()
+    min_x, min_y = (min(c.x for c in flipped_tiles), min(c.y for c in flipped_tiles))
+    max_x, max_y = (max(c.x for c in flipped_tiles), max(c.y for c in flipped_tiles))
+    for i in range(min_x - 1, max_x + 2):
+        for j in range(min_y - 1, max_y + 2):
+            coord = Coord(i, j)
+            num_black_adjacent_tiles = find_black_adjacent_tiles(coord, flipped_tiles)
+            is_black_tile = coord in flipped_tiles
+            if is_black_tile and (num_black_adjacent_tiles == 0 or num_black_adjacent_tiles > 2):
+                updated_flipped_tiles.remove(coord)
+            elif not is_black_tile and num_black_adjacent_tiles == 2:
+                updated_flipped_tiles.add(coord)
+    return updated_flipped_tiles
 
 
 # Axial coords - https://www.redblobgames.com/grids/hexagons/#coordinates-axial
@@ -41,3 +63,13 @@ def update_tiles(coord: Coord, flipped_tiles: Set[Coord]):
         flipped_tiles.remove(coord)
     else:
         flipped_tiles.add(coord)
+
+
+def find_black_adjacent_tiles(coord: Coord, flipped_tiles: Set[Coord]) -> int:
+    adjacent_tiles = {Coord(coord.x, coord.y + 1),
+                      Coord(coord.x - 1, coord.y + 1),
+                      Coord(coord.x, coord.y - 1),
+                      Coord(coord.x + 1, coord.y - 1),
+                      Coord(coord.x + 1, coord.y),
+                      Coord(coord.x - 1, coord.y)}
+    return len(set.intersection(adjacent_tiles, flipped_tiles))
--- tests/day24/test_lobby_layout.py
+++ tests/day24/test_lobby_layout.py
@@ -3,15 +3,26 @@ import re
 from pathlib import Path
 from typing import List
 
-from main.day24.lobby_layout import solve
+import pytest
+
+from main.day24.lobby_layout import solve_place_tiles, solve_update_tiles
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 10
+def test_place_tiles_simple():
+    assert len(solve_place_tiles(read_input("data/test_input.txt"))) == 10
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 244
+def test_place_tiles_real():
+    assert len(solve_place_tiles(read_input("data/input.txt"))) == 244
+
+
+def test_update_tiles_simple():
+    assert len(solve_update_tiles(read_input("data/test_input.txt"), 100)) == 2208
+
+
+@pytest.mark.skip(reason="Takes about 5s")
+def test_update_tiles_real():
+    assert len(solve_update_tiles(read_input("data/input.txt"), 100)) == 3665
 
 
 def read_input(file_name) -> List[List[str]]:
