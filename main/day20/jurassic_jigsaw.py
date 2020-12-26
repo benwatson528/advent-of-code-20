17d5a6f263cb3294c8695d6b847a2bd5f11bf190
Ben Watson <benwatson528@gmail.com>
2020-12-26 16:45:32 +0000

    Day 20

--- main/day20/jurassic_jigsaw.py
+++ main/day20/jurassic_jigsaw.py
@@ -1,9 +1,47 @@
-from typing import Dict
-
-from main.day20.tile import Tile
+from typing import Dict, List, Set
 
 
-def solve(tiles: Dict[int, Tile]) -> int:
-    return 5
+def solve(raw_tiles: Dict[int, List[str]]) -> Set[int]:
+    all_sides = get_all_sides(raw_tiles)
+    corner_tiles = find_corner_tiles(all_sides)
+    return corner_tiles
 
-# Build up a List[(tile_id, str)] for every possible edge and orientation
+
+def find_corner_tiles(tiles: Dict[int, List[str]]) -> Set[int]:
+    corner_tiles = set()
+    for tile_id, sides in tiles.items():
+        # Only compare against the original 4 sides - the flipped sides are accounted for in the main tiles list
+        num_unique_sides = sum(1 for i in range(4) if is_unique_side(sides[i], tile_id, tiles))
+        if num_unique_sides == 2:
+            corner_tiles.add(tile_id)
+    return corner_tiles
+
+
+def get_column(tile: List[str], idx: int) -> List[str]:
+    return [row[idx] for row in tile]
+
+
+def is_unique_side(side: str, tile_id: int, tiles: Dict[int, List[str]]) -> bool:
+    for iter_tile_id, iter_sides in tiles.items():
+        if iter_tile_id == tile_id:
+            continue
+        for iter_side in iter_sides:
+            if side == iter_side:
+                return False
+    return True
+
+
+def get_all_sides(tiles: Dict[int, List[str]]) -> Dict[int, List[str]]:
+    all_sides = {}
+    for tile_id, tile in tiles.items():
+        left_side = ''.join(get_column(tile, 0))
+        left_side_reversed = ''.join(left_side[::-1])
+        right_side = ''.join(get_column(tile, len(tile) - 1))
+        right_side_reversed = ''.join(right_side[::-1])
+        top = tile[0]
+        top_reversed = top[::-1]
+        bottom = tile[-1]
+        bottom_reversed = bottom[::-1]
+        all_sides[tile_id] = [left_side, right_side, top, bottom, left_side_reversed, right_side_reversed, top_reversed,
+                              bottom_reversed]
+    return all_sides
--- tests/day20/test_jurassic_jigsaw.py
+++ tests/day20/test_jurassic_jigsaw.py
@@ -1,27 +1,25 @@
+import math
 import os
 import re
 from collections import defaultdict
 from pathlib import Path
 from typing import List, Dict
 
-import pytest
-
 from main.day20.jurassic_jigsaw import solve
-from main.day20.tile import Tile
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 3
+    corner_tiles = solve(read_input("data/test_input.txt"))
+    assert math.prod(corner_tiles) == 20899048083289
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_real():
-    assert solve(read_input("data/input.txt")) == 3
+    corner_tiles = solve(read_input("data/input.txt"))
+    assert math.prod(corner_tiles) == 15003787688423
 
 
-def read_input(file_name: str) -> Dict[int, Tile]:
-    tiles = defaultdict(Tile)
+def read_input(file_name: str) -> Dict[int, List[str]]:
+    tiles = defaultdict(List[str])
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         for line in f:
             stripped_line = line.strip('\n')
@@ -29,13 +27,9 @@ def read_input(file_name: str) -> Dict[int, Tile]:
                 tile_id = int(re.search(r'\d+', stripped_line).group())
                 tile = []
             elif len(stripped_line) == 0:
-                tiles[tile_id] = parse_tile(tile_id, tile)
+                tiles[tile_id] = tile
                 tile = []
             else:
                 tile.append(stripped_line)
-        tiles[tile_id] = parse_tile(tile_id, tile)
+        tiles[tile_id] = tile
         return tiles
-
-
-def parse_tile(tile_id: int, raw_tile: List[str]):
-    return Tile(tile_id, raw_tile[0], raw_tile[-1], raw_tile[0], raw_tile[0], 0, False, raw_tile)
