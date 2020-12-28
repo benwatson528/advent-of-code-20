5456bfef638ecf784462070c2998786c31a9fd7b
Ben Watson <benwatson528@gmail.com>
2020-12-28 18:41:45 +0000

    Day 19 part 1

--- main/day19/monster_messages.py
+++ main/day19/monster_messages.py
@@ -1,36 +1,42 @@
 import re
-from typing import Dict, List
+from typing import Dict, List, Set
 
 rules = {}
 
 
-def solve(r: Dict[str, str]) -> List[str]:
+def solve(r: Dict[str, str]) -> Set[str]:
     global rules
     rules = r
-    possible_messages = build_possible_messages(rules['0'], [])
-    return possible_messages
+    return set(build_possible_messages(rules['0'], ['']))
+
+
+def build_regex(start_rule_value: str, messages: List[str]) -> str:
+    return ""
 
 
 # Do we need a concept of finished messages vs in-progress messages? Not sure, what about ORs?
-def build_possible_messages(rule_value: str, messages: List[str]) -> List[str]:
+def build_possible_messages(rule_value: str, suffixes: List[str]) -> List[str]:
     regex = re.findall(r'\d+', rule_value)
-    if "\"" in rule_value:
-        # Just return a single value (as a singleton list)
-        value_ = [rule_value[1]]
-        return value_
-    elif "|" in rule_value:
-        # Go down the left - this gives us numerous new endings
-        lhs = build_possible_messages(regex[0] + " " + regex[1], messages)
-        # Go down the right - this gives us numerous new endings
-        rhs = build_possible_messages(regex[2] + " " + regex[3], messages)
-        # We should end up returning 2 new messages here - our existing prefix + LHS, our existing prefix + RHS
-        combined = messages + lhs + rhs
-        return combined
+    if '"' in rule_value:
+        updated_suffixes = []
+        for suffix in suffixes:
+            updated_suffixes.append(suffix + rule_value[1])
+        return updated_suffixes
+    elif '|' in rule_value:
+        updated_suffixes = []
+        if len(regex) == 4:
+            lhs_suffixes = build_possible_messages(regex[0] + " " + regex[1], suffixes)
+            rhs_suffixes = build_possible_messages(regex[2] + " " + regex[3], suffixes)
+        else:
+            lhs_suffixes = build_possible_messages(regex[0], suffixes)
+            rhs_suffixes = build_possible_messages(regex[1], suffixes)
+        for lhs in lhs_suffixes:
+            updated_suffixes.append(lhs)
+        for rhs in rhs_suffixes:
+            updated_suffixes.append(rhs)
+        return updated_suffixes
     else:
-        # We
-        ans = []
+        updated_suffixes = suffixes.copy()
         for r in regex:
-            ans += build_possible_messages(rules[r], messages)
-            # Get a lot of suffixes and slap them onto our existing message. But what if multiple ORs inside...
-        joined = ''.join(ans)
-        return [joined]
+            updated_suffixes = [s for s in build_possible_messages(rules[r], updated_suffixes)]
+    return updated_suffixes
--- tests/day19/test_monster_messages.py
+++ tests/day19/test_monster_messages.py
@@ -2,12 +2,9 @@ import os
 from pathlib import Path
 from typing import Dict
 
-import pytest
-
 from main.day19.monster_messages import solve
 
 
-@pytest.mark.skip(reason="Fails")
 def test_simple():
     rules, messages = read_input("data/test_input.txt")
     possible_messages = solve(rules)
@@ -15,10 +12,11 @@ def test_simple():
     assert len(valid_messages) == 2
 
 
-@pytest.mark.skip(reason="Fails")
 def test_real():
     rules, messages = read_input("data/input.txt")
-    assert solve(rules) == 3
+    possible_messages = solve(rules)
+    valid_messages = [x for x in messages if x in possible_messages]
+    assert len(valid_messages) == 208
 
 
 def read_input(file_name: str) -> (Dict[int, str], str):
