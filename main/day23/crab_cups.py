cb439dc415d9ad6afe2277cdb0b9b81b9e57906b
Ben Watson <benwatson528@gmail.com>
2020-12-29 17:15:35 +0000

    Day 23 part 2

--- main/day23/crab_cups.py
+++ main/day23/crab_cups.py
@@ -1,42 +1,32 @@
 from typing import List, Dict
 
-num_cups = 0
-num_turns = 0
+
+def solve(cups: List[int], num_turns: int) -> Dict[int, int]:
+    cups_map = dict(zip(cups, cups[1:]))
+    cups_map[cups[-1]] = cups[0]
+    return take_turn(cups_map, cups[0], num_turns)
 
 
-def solve(cups: str, turns: int) -> Dict[int, int]:
-    global num_cups
+def take_turn(cups: Dict[int, int], current_cup: int, num_turns: int) -> Dict[int, int]:
     num_cups = len(cups)
-    global num_turns
-    num_turns = turns
-    cup_ints = [int(c) for c in cups]
-    cups_map = dict(zip(cup_ints, cup_ints[1:]))
-    cups_map[cup_ints[-1]] = cup_ints[0]
-    return take_turn(cups_map, cup_ints[0], 1)
-
-
-def take_turn(cups: Dict[int, int], current_cup: int, current_turn: int) -> Dict[int, int]:
-    # print_cups(cups, current_cup, current_turn)
-    if current_turn > num_turns:
-        return cups
-    else:
+    for _ in range(num_turns):
         cups_to_move = [cups[current_cup], cups[cups[current_cup]], cups[cups[cups[current_cup]]]]
-        destination_cup = find_destination_cup(current_cup, cups_to_move)
+        destination_cup = find_destination_cup(current_cup, cups_to_move, num_cups)
         cups[current_cup] = cups[cups_to_move[-1]]
         cups[cups_to_move[-1]] = cups[destination_cup]
         cups[destination_cup] = cups_to_move[0]
-        next_cup = cups[current_cup]
-        return take_turn(cups, next_cup, current_turn + 1)
+        current_cup = cups[current_cup]
+    return cups
 
 
-def find_destination_cup(current_cup: int, moved_cups: List[int]) -> int:
+def find_destination_cup(current_cup: int, moved_cups: List[int], num_cups: int) -> int:
     destination_cup = current_cup - 1
     if destination_cup == 0:
-        destination_cup = 9
+        destination_cup = num_cups
     while destination_cup in moved_cups:
         destination_cup -= 1
         if destination_cup < 1:
-            destination_cup = 9
+            destination_cup = num_cups
     return destination_cup
 
 
@@ -45,14 +35,3 @@ def follow_n_cups(current_cup: int, cups: Dict[int, int], n: int) -> int:
     for _ in range(n - 1):
         next_cup = cups[next_cup]
     return cups[next_cup]
-
-
-def print_cups(cups: Dict[int, int], current_cup: int, turn: int):
-    print(f"\nTurn {turn}")
-    next_cup = 1
-    for _ in range(1, 10):
-        if next_cup == current_cup:
-            print(f"({next_cup})", end=' ')
-        else:
-            print(f"{next_cup}", end=' ')
-        next_cup = cups[next_cup]
--- tests/day23/test_crab_cups.py
+++ tests/day23/test_crab_cups.py
@@ -1,26 +1,32 @@
 from typing import Dict
 
+import pytest
+
 from main.day23.crab_cups import solve
 
 
 def test_part_one_simple():
-    end_cups = solve("389125467", 100)
+    end_cups = solve([int(c) for c in "389125467"], 100)
     assert pivot_answer(end_cups) == "67384529"
 
 
 def test_part_one_real():
-    end_cups = solve("219347865", 100)
+    end_cups = solve([int(c) for c in "219347865"], 100)
     assert pivot_answer(end_cups) == "36472598"
 
 
+@pytest.mark.skip(reason="Takes about 11s")
 def test_part_two_simple():
-    end_cups = solve("389125467", 100)
-    assert pivot_answer(end_cups) == "67384529"
+    cups = [int(c) for c in "389125467"] + list(range(10, 1000001))
+    end_cups = solve(cups, 10000000)
+    assert end_cups[1] * end_cups[end_cups[1]] == 149245887792
 
 
+@pytest.mark.skip(reason="Takes about 11s")
 def test_part_two_real():
-    end_cups = solve("219347865", 100)
-    assert pivot_answer(end_cups) == "36472598"
+    cups = [int(c) for c in "219347865"] + list(range(10, 1000001))
+    end_cups = solve(cups, 10000000)
+    assert end_cups[1] * end_cups[end_cups[1]] == 90481418730
 
 
 def pivot_answer(end_cups: Dict[int, int]) -> str:
