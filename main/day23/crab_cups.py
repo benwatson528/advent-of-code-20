ca4b30febe96371e3d7ccbd4c626e53a7562cf08
Ben Watson <benwatson528@gmail.com>
2020-12-29 16:52:51 +0000

    Day 23 part 1 refactored

--- main/day23/crab_cups.py
+++ main/day23/crab_cups.py
@@ -1,39 +1,58 @@
-from typing import List
+from typing import List, Dict
 
 num_cups = 0
+num_turns = 0
 
 
-def solve(cups: str, num_turns: int) -> List[int]:
+def solve(cups: str, turns: int) -> Dict[int, int]:
     global num_cups
     num_cups = len(cups)
-    return take_turn([int(c) for c in cups], 0, 1, num_turns)
+    global num_turns
+    num_turns = turns
+    cup_ints = [int(c) for c in cups]
+    cups_map = dict(zip(cup_ints, cup_ints[1:]))
+    cups_map[cup_ints[-1]] = cup_ints[0]
+    return take_turn(cups_map, cup_ints[0], 1)
 
 
-def take_turn(cups: List[int], current_cup_idx: int, current_turn: int, num_turns: int) -> List[int]:
+def take_turn(cups: Dict[int, int], current_cup: int, current_turn: int) -> Dict[int, int]:
+    # print_cups(cups, current_cup, current_turn)
     if current_turn > num_turns:
         return cups
     else:
-        removed_cups = [cups[c] for c in [(current_cup_idx + 1) % num_cups,
-                                          (current_cup_idx + 2) % num_cups,
-                                          (current_cup_idx + 3) % num_cups]]
-        current_cup = cups[current_cup_idx]
-        destination_cup = find_destination_cup(current_cup, removed_cups)
-        updated_cups = []
-        for idx in range(num_cups):
-            if cups[idx] not in removed_cups:
-                updated_cups.append(cups[idx])
-            if cups[idx] == destination_cup:
-                updated_cups.extend(removed_cups)
-        next_cup_idx = (updated_cups.index(current_cup) + 1) % num_cups
-        return take_turn(updated_cups, next_cup_idx, current_turn + 1, num_turns)
+        cups_to_move = [cups[current_cup], cups[cups[current_cup]], cups[cups[cups[current_cup]]]]
+        destination_cup = find_destination_cup(current_cup, cups_to_move)
+        cups[current_cup] = cups[cups_to_move[-1]]
+        cups[cups_to_move[-1]] = cups[destination_cup]
+        cups[destination_cup] = cups_to_move[0]
+        next_cup = cups[current_cup]
+        return take_turn(cups, next_cup, current_turn + 1)
 
 
-def find_destination_cup(current_cup: int, removed_cups: List[int]) -> int:
+def find_destination_cup(current_cup: int, moved_cups: List[int]) -> int:
     destination_cup = current_cup - 1
     if destination_cup == 0:
         destination_cup = 9
-    while destination_cup in removed_cups:
+    while destination_cup in moved_cups:
         destination_cup -= 1
         if destination_cup < 1:
             destination_cup = 9
     return destination_cup
+
+
+def follow_n_cups(current_cup: int, cups: Dict[int, int], n: int) -> int:
+    next_cup = current_cup
+    for _ in range(n - 1):
+        next_cup = cups[next_cup]
+    return cups[next_cup]
+
+
+def print_cups(cups: Dict[int, int], current_cup: int, turn: int):
+    print(f"\nTurn {turn}")
+    next_cup = 1
+    for _ in range(1, 10):
+        if next_cup == current_cup:
+            print(f"({next_cup})", end=' ')
+        else:
+            print(f"{next_cup}", end=' ')
+        next_cup = cups[next_cup]
--- tests/day23/test_crab_cups.py
+++ tests/day23/test_crab_cups.py
@@ -1,27 +1,32 @@
+from typing import Dict
+
 from main.day23.crab_cups import solve
 
 
 def test_part_one_simple():
     end_cups = solve("389125467", 100)
-    assert pivot_answer(end_cups) == 67384529
+    assert pivot_answer(end_cups) == "67384529"
 
 
 def test_part_one_real():
     end_cups = solve("219347865", 100)
-    assert pivot_answer(end_cups) == 36472598
+    assert pivot_answer(end_cups) == "36472598"
 
 
 def test_part_two_simple():
     end_cups = solve("389125467", 100)
-    assert pivot_answer(end_cups) == 67384529
+    assert pivot_answer(end_cups) == "67384529"
 
 
 def test_part_two_real():
     end_cups = solve("219347865", 100)
-    assert pivot_answer(end_cups) == 36472598
+    assert pivot_answer(end_cups) == "36472598"
 
 
-def pivot_answer(end_cups):
-    pivot_idx = end_cups.index(1)
-    pivoted_cups = end_cups[pivot_idx + 1:] + end_cups[:pivot_idx]
-    return int(''.join(str(i) for i in pivoted_cups))
+def pivot_answer(end_cups: Dict[int, int]) -> str:
+    ans = ""
+    next_cup = 1
+    for _ in range(1, 9):
+        next_cup = end_cups[next_cup]
+        ans += str(next_cup)
+    return ans
